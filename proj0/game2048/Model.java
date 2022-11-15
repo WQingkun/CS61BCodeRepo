df1be063f31bee496e684fc0becdc255114d3dc4
WQingkun <gvhbjn@126.com>
2022-11-15 20:15:13 +0800

    Project 0 finished!

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -106,15 +106,298 @@ public class Model extends Observable {
      *    value, then the leading two tiles in the direction of motion merge,
      *    and the trailing tile does not.
      * */
+    public boolean CheckAndMerge(int cnt, int[] ColArray,int col) {
+        if (cnt == 0) {
+            //a ,a ,b ,b a,b != 0
+            if (ColArray[0] == ColArray[1] && ColArray[2] == ColArray[3]) {
+                Tile t = board.tile(col, 2);
+                board.move(col, 3, t);
+                t = board.tile(col, 0);
+                board.move(col, 2, t);
+                t = board.tile(col, 1);
+                board.move(col, 2, t);
+                score = score + ColArray[0] * 2 + ColArray[2] * 2;
+                return true;
+            }
+            //a b b c
+            if (ColArray[0] != ColArray[1] && ColArray[1] == ColArray[2] && ColArray[2] != ColArray[3]) {
+                Tile t = board.tile(col, 1);
+                board.move(col, 2, t);
+                t = board.tile(col, 0);
+                board.move(col, 1, t);
+                score += 2 * ColArray[1];
+                return true;
+            }
+            //a b c c
+            if (ColArray[0] != ColArray[1] && ColArray[1] != ColArray[2] && ColArray[2] == ColArray[3]) {
+                Tile t = board.tile(col, 0);
+                board.move(col, 1, t);
+                score += 2 * ColArray[2];
+                return true;
+            }
+            //a a b c
+            if (ColArray[0] == ColArray[1] && ColArray[2] != ColArray[3]) {
+                Tile t = board.tile(col, 2);
+                board.move(col, 3, t);
+                t = board.tile(col, 1);
+                board.move(col, 2, t);
+                t = board.tile(col, 0);
+                board.move(col, 1, t);
+                score += 2 * ColArray[0];
+                return true;
+            }
+            return false;
+        }
+        if (cnt == 1){
+           if (ColArray[0] == 0){
+               // 0 a a b
+               if(ColArray[1] == ColArray[2]){
+                   Tile t = board.tile(col, 2);
+                   board.move(col, 3, t);
+                   t = board.tile(col, 1);
+                   board.move(col, 3, t);
+                   t = board.tile(col, 0);
+                   board.move(col, 2, t);
+                   score += 2*ColArray[2];
+                   return true;
+               } else if (ColArray[0] == ColArray[1]) {
+                   // 0 a b b
+                   Tile t = board.tile(col, 2);
+                   board.move(col, 3, t);
+                   t = board.tile(col, 1);
+                   board.move(col,2, t);
+                   t = board.tile(col, 0);
+                   board.move(col, 2, t);
+                   score += 2*ColArray[0];
+                   return true;
+               }else{
+                   //0 a b c
+                   Tile t = board.tile(col, 2);
+                   board.move(col, 3, t);
+                   t = board.tile(col, 1);
+                   board.move(col, 2, t);
+                   t = board.tile(col, 0);
+                   board.move(col, 1, t);
+                   return true;
+               }
+           }
+           if (ColArray[1] == 0){
+               if(ColArray[0] == ColArray[2]){
+                   //a 0 a b
+                   Tile t = board.tile(col, 1);
+                   board.move(col, 3, t);
+                   t = board.tile(col, 0);
+                   board.move(col, 2, t);
+                   score += 2*ColArray[0];
+                   return true;
+               }else if(ColArray[2] == ColArray[3]){
+                   // a 0 b b
+                   Tile t = board.tile(col, 1);
+                   board.move(col, 2, t);
+                   t = board.tile(col, 0);
+                   board.move(col, 2, t);
+                   score += 2*ColArray[2];
+                   return true;
+               }else {
+                   // a 0 b c
+                   Tile t = board.tile(col, 1);
+                   board.move(col, 2, t);
+                   t = board.tile(col, 0);
+                   board.move(col, 1, t);
+                   return true;
+               }
+
+           }
+           if (ColArray[2] == 0){
+               if(ColArray[0] == ColArray[1]){
+                   //a a 0 b
+                   Tile t = board.tile(col, 2);
+                   board.move(col, 3, t);
+                   t = board.tile(col, 0);
+                   board.move(col, 2, t);
+                   score += 2*ColArray[0];
+                   return true;
+               }else if(ColArray[1] == ColArray[3]){
+                   //a b 0 b
+                   Tile t = board.tile(col, 0);
+                   board.move(col, 2, t);
+                   score += 2*ColArray[1];
+                   return true;
+               }else{
+                   //a b 0 c
+                   Tile t = board.tile(col, 0);
+                   board.move(col, 1, t);
+                   return true;
+               }
+           }
+           if (ColArray[3] == 0){
+               //a a b 0
+               if(ColArray[0] == ColArray[1]){
+                   Tile t = board.tile(col, 2);
+                   board.move(col, 3, t);
+                   t = board.tile(col, 1);
+                   board.move(col, 2, t);
+                   score += 2*ColArray[0];
+                   return true;
+               }else if(ColArray[1] == ColArray[2]){
+                   //a b b 0
+                   Tile t = board.tile(col, 1);
+                   board.move(col, 2, t);
+                   score += 2*ColArray[1];
+                   return true;
+               }else{
+                   // a b c 0
+                   return false;
+               }
+           }
+        }
+        if (cnt == 2){
+            if(ColArray[0] == ColArray[1] && ColArray[0] == 0){
+                if(ColArray[2] == ColArray[3]){
+                    // 0 0 a a
+                    Tile t = board.tile(col, 1);
+                    board.move(col, 3, t);
+                    t = board.tile(col, 0);
+                    board.move(col, 3, t);
+                    score += 2*ColArray[2];
+                    return true;
+                }else{
+                    // 0 0 a b
+                    Tile t = board.tile(col, 1);
+                    board.move(col, 3, t);
+                    t = board.tile(col, 0);
+                    board.move(col, 2, t);
+                    return true;
+                }
+            }
+            if(ColArray[0] == ColArray[2] && ColArray[0] == 0){
+                if(ColArray[1] == ColArray[3]){
+                    //0 a 0 a
+                    Tile t = board.tile(col, 2);
+                    board.move(col, 3, t);
+                    t = board.tile(col, 0);
+                    board.move(col, 3, t);
+                    score += 2*ColArray[1];
+                    return true;
+                }else{
+                    //0 a 0 b
+                    Tile t = board.tile(col, 2);
+                    board.move(col, 3, t);
+                    t = board.tile(col, 0);
+                    board.move(col, 2, t);
+                    return true;
+                }
+            }
+            if(ColArray[0] == ColArray[3] && ColArray[0] == 0){
+                if(ColArray[1] == ColArray[2]){
+                    //0 a a 0
+                    Tile t = board.tile(col, 2);
+                    board.move(col, 3, t);
+                    t = board.tile(col, 1);
+                    board.move(col, 3, t);
+                    score += 2*ColArray[1];
+                    return true;
+                }else{
+                    //0 a b 0
+                    Tile t = board.tile(col, 2);
+                    board.move(col, 3, t);
+                    t = board.tile(col, 1);
+                    board.move(col, 2, t);
+                    return true;
+                }
+
+            }
+            if(ColArray[1] == ColArray[2] && ColArray[1] == 0){
+                if(ColArray[0] == ColArray[3]){
+                    //a 0 0 a
+                    Tile t = board.tile(col, 0);
+                    board.move(col, 3, t);
+                    score += 2*ColArray[0];
+                }else{
+                    //a 0 0 b
+                    Tile t = board.tile(col, 0);
+                    board.move(col, 2, t);
+                }
+                return true;
+            }
+            if(ColArray[1] == ColArray[3] && ColArray[1] == 0){
+                Tile t;
+                if(ColArray[0] == ColArray[2]){
+                    //a 0 a 0
+                    t = board.tile(col, 1);
+                    board.move(col, 3, t);
+                    score += 2*ColArray[0];
+                }else{
+                    //a 0 b 0
+                    t = board.tile(col, 1);
+                    board.move(col, 2, t);
+                }
+                return true;
+            }
+            if(ColArray[2] == ColArray[3] && ColArray[2] == 0){
+                Tile t;
+                if(ColArray[0] == ColArray[1]){
+                    //a a 0 0
+                    t = board.tile(col, 2);
+                    board.move(col, 3, t);
+                    score += 2*ColArray[0];
+                    return true;
+                }else {
+                    //a b 0 0
+                    return false;
+                }
+            }
+
+        }
+        if (cnt == 3){
+            if(ColArray[0] != 0){
+                //a 0 0 0
+                return false;
+            }else {
+                for(int i = 1;i < size(); i += 1){
+                   if (ColArray[i] != 0){
+                       Tile t = board.tile(col, 3 - i);
+                       board.move(col, 3, t);
+                       return true;
+                   }
+                }
+            }
+        }
+        return false;
+    }
+
+
+    public boolean ColOperation(int col, int size){
+        //Read col values of board
+        int cnt = 0;
+        int[] ColArray = new int[4];
+        for(int i = 0; i < size; i += 1){
+            if(board.tile(col, 3 - i) == null){
+                ColArray[i] = 0;
+                cnt += 1;
+            }else{
+                ColArray[i] = board.tile(col, 3 - i).value();
+            }
+        }
+        return CheckAndMerge(cnt,ColArray,col);
+    }
     public boolean tilt(Side side) {
         boolean changed;
         changed = false;
-
+        board.setViewingPerspective(side);
         // TODO: Modify this.board (and perhaps this.score) to account
         // for the tilt to the Side SIDE. If the board changed, set the
         // changed local variable to true.
+        int i = 0;
+        while(i < size()){
+            if(ColOperation(i, size())){
+                changed = true;
 
+            }
+            i += 1;
+        }
         checkGameOver();
+        board.setViewingPerspective(Side.NORTH);
         if (changed) {
             setChanged();
         }
@@ -137,7 +420,18 @@ public class Model extends Observable {
      *  Empty spaces are stored as null.
      * */
     public static boolean emptySpaceExists(Board b) {
-        // TODO: Fill in this function.
+        int i = 0;
+        int size = b.size();
+        while(i < size){
+            int j = 0;
+            while(j < size){
+                if (b.tile(i, j) == null){
+                    return true;
+                }
+                j += 1;
+            }
+            i += 1;
+        }
         return false;
     }
 
@@ -147,7 +441,18 @@ public class Model extends Observable {
      * given a Tile object t, we get its value with t.value().
      */
     public static boolean maxTileExists(Board b) {
-        // TODO: Fill in this function.
+        int i = 0;
+        int size = b.size();
+        while (i < size){
+            int j = 0;
+            while (j < size){
+                if (b.tile(i, j) != null && b.tile(i, j).value() == MAX_PIECE){
+                    return  true;
+                }
+                j += 1;
+            }
+            i += 1;
+        }
         return false;
     }
 
@@ -157,9 +462,95 @@ public class Model extends Observable {
      * 1. There is at least one empty space on the board.
      * 2. There are two adjacent tiles with the same value.
      */
-    public static boolean atLeastOneMoveExists(Board b) {
-        // TODO: Fill in this function.
+    public static boolean CheckSides(int row, int col, Board b){
+        int size = b.size();
+        if (row == 0){
+            int j = 0;
+            while (j < size){
+                if (size >= 2 && b.tile(j,0).value() == b.tile(j, row + 1).value()){
+                    return true;
+                }
+                /*Check if there is same value in row 0*/
+                if (j+1 < size && b.tile(j, row).value() == b.tile(j + 1, row).value()){
+                    return true;
+                }
+                j += 1;
+            }
+        }
+        if (row == size - 1){
+            int j = 0;
+            while (j < size){
+                if (size >= 2 && b.tile(j,row).value() == b.tile(j, row - 1).value()){
+                    return true;
+                }
+                if (j+1 < size && b.tile(j, row).value() == b.tile(j + 1, row).value()){
+                    return true;
+                }
+                j += 1;
+            }
+        }
+        if (col == size - 1){
+            int j = 0;
+            while (j < size){
+                if (size >= 2 && b.tile(col,j).value() == b.tile(col - 1, j).value()){
+                    return true;
+                }
+                if (j+1 < size && b.tile(col, j).value() == b.tile(col, j + 1).value()){
+                    return true;
+                }
+                j += 1;
+            }
+        }
+        if (col == 0){
+            int j = 0;
+            while (j < size){
+                if (size >= 2 && b.tile(col,j).value() == b.tile(col + 1, j).value()){
+                    return true;
+                }
+                if (j+1 < size && b.tile(col, j).value() == b.tile(col, j + 1).value()){
+                    return true;
+                }
+                j += 1;
+            }
+        }
         return false;
+    }
+    public static boolean TwoAjacent(Board b){
+        int i = 0;
+        int size = b.size();
+        while (i < size){
+            int j = 0;
+            while (j < size){
+                if (i == 0 || i == size - 1 || j == 0 || j == size - 1){
+                    if (CheckSides(i , j , b)){
+                        return true;
+                    }
+                }
+                else{
+                    if (b.tile(i, j).value() == b.tile(i, j - 1).value()){
+                        return true;
+                    }
+                    if (b.tile(i, j).value() == b.tile(i, j + 1).value()){
+                        return true;
+                    }
+                    if (b.tile(i, j).value() == b.tile(i - 1, j).value()){
+                        return true;
+                    }
+                    if (b.tile(i, j).value() == b.tile(i + 1, j).value()){
+                        return true;
+                    }
+                }
+                j += 1;
+            }
+            i += 1;
+        }
+        return false;
+    }
+    public static boolean atLeastOneMoveExists(Board b) {
+        if (emptySpaceExists((b))){
+            return true;
+        }
+        return TwoAjacent(b);
     }
 
 
